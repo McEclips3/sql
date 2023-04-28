5f2afe701390c52efcad5d512dcc34803967bc11
McEclips3 <125393868+McEclips3@users.noreply.github.com>
2023-04-28 21:09:35 +0300

    Add files via upload

---
+++ Script-1.sql
@@ -0,0 +1,54 @@
+create table if not exists types_of_music
+(
+type_id SERIAL primary key,
+name VARCHAR(40) not NULL
+ );
+ 
+create table if not exists music_artist
+(
+artist_id serial primary key,
+name varchar(40) not null
+); 
+
+create table if not exists types_artist
+(
+type_id integer references types_of_music(type_id),
+artist_id integer references music_artist(artist_id),
+constraint ta primary key (type_id, artist_id)
+);
+
+create table if not exists album
+(
+album_id serial primary key,
+name varchar(40) not null,
+year integer not null
+);
+
+create table if not exists artist_album
+(
+artist_id integer references music_artist(artist_id),
+album_id integer references album(album_id),
+constraint al primary key (artist_id, album_id)
+);
+
+create table if not exists song
+(
+song_id serial primary key,
+album_id integer not null references album(album_id),
+name varchar(40) not null,
+duration varchar(40) not null
+);
+
+create table if not exists collection
+(collection_id serial primary key,
+name varchar(40) not null,
+year date not null
+);
+
+create table if not exists collection_song
+(collection_id integer references collection(collection_id),
+song_id integer references song(song_id),
+constraint sc primary key(collection_id, song_id)
+);
+
+
